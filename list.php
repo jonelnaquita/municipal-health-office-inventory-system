6caada50e898cd8dbd4ba268b4ddf1d488974369
D'CODER <60844924+jonelnaquita@users.noreply.github.com>
2023-12-30 15:50:25 +0800

    modify-export

--- list.php
+++ list.php
@@ -206,23 +206,6 @@ $('#new_product').click(function(){
                     columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] // Include the first and third columns
                 },
             },
-            {
-                extend: 'print',
-                text: 'Print all',
-                exportOptions: {
-                    modifier: { 
-                        selected: null
-                    },
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] // Include the first and third columns
-                },
-            },
-            {
-                extend: 'print',
-                text: 'Print selected'
-                exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] // Include the first and third columns
-                },
-            },
             'colvis'
         ],
         select: true,
